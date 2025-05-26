50dd363ce16dee6fc9285753f9f64e4c416ddce4
Luu Hoang Duc <lhduc141@gmail.com>
2025-05-26 16:34:50 +0700

    fix: AuthService Api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import {LoggerMiddleware} from "./middleware/logging.middleware";
 import {MainModule} from "./modules/main.module";
+import {AuthService} from "./modules/auth/auth.service";
 
 @Module({
   imports: [MainModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
+  controllers: [AppController],
+  providers: [AppService, AuthService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
